f9055c412f25b3b3af52cbe7597e4760f45da326
BryanCorona0524 <103693344+BryanCorona0524@users.noreply.github.com>
2022-05-01 00:13:52 -0500

    Update Board.java

--- board/Board.java
+++ board/Board.java
@@ -5,5 +5,6 @@ public class Board {
 	public static void main(String args[])
 	{
 		System.out.println("hello");
+		// this will probably be named to game.java and will house the methods that read the csv files and also create the players/start the game
 	}
 }
