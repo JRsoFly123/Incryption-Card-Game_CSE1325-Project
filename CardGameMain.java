661059b906d2c6f54982b6ad3bb03edc8dfb3f6d
JRsoFly123 <63214250+JRsoFly123@users.noreply.github.com>
2022-05-03 16:07:30 -0500

    Add files via upload
    v1 - Basic version (Only BoardUI used for GUI testing)

---
+++ CardGameMain.java
@@ -0,0 +1,17 @@
+import board.*;
+
+public class CardGameMain {
+    public static void main(String args[])
+    {
+        // Before starting the game we need the player one and two name!
+        // That mean we need to start the game from the start menu...
+
+        // Starting the game here
+        Game currentGame = new Game();
+        currentGame.startNewGame("player 1", "player 2", "UnitDeck.csv");
+        javax.swing.SwingUtilities.invokeLater(new Runnable()
+        {
+            public void run() {BoardUI.createAndShowGUI(currentGame.getPlayer(),"Wizard");}
+        });
+    }
+}
